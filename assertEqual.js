9c9bcf467d7c3e484fe1f55047f51b3e73376750
goingdust <dustinluck@gmail.com>
2021-11-23 22:28:48 +0000

    Changed assertEqual() so that the emojis work now

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function(actual, expected) {
-  const pass = 0x2705;
-  const fail = 0x274C;
+  const pass = '✅✅✅';
+  const fail = '❌❌❌';
   
   if (actual === expected) {
-    return console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+    return console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
