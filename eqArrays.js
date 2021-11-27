b851fd79ad495259873887526c5038d5a87ae02e
goingdust <dustinluck@gmail.com>
2021-11-27 02:01:36 +0000

    Inserted a conditional into eqArrays that checks whether nested arrays are equal using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -10,15 +10,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arg1, arg2) {
+  let equality;
   if (arg1.length !== arg2.length) {
     return false;
   }
   for (let i = 0; i < arg1.length; i++) {
-    if (arg1[i] !== arg2[i]) {
+    if (arg1[i] === arg2[i]) {
+      equality = true;
+    } else if (Array.isArray(arg1[i]) && Array.isArray(arg2[i]) && eqArrays(arg1[i], arg2[i])) {
+      equality = true;
+    } else {
       return false;
     }
   }
-  return true;
+  return equality;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
@@ -26,3 +31,8 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([45, 50], [40, 50]), false);
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
