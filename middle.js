f2c820f8566cb247017f57d66eb08cf29d490d7b
goingdust <dustinluck@gmail.com>
2021-11-29 15:43:08 +0000

    Refactored middle to remove wet code and moved tests to middleTest

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arg1, arg2) {
-  if (arg1.length !== arg2.length) {
-    return false;
-  }
-  for (let i = 0; i < arg1.length; i++) {
-    if (arg1[i] !== arg2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    return console.log('✅✅✅ Assertion Passed: Array #1 === Array #2');
-  } else {
-    return console.log('❌❌❌ Assertion Failed: Array #1 !== Array #2');
-  }
-};
-
 const middle = function(array) {
   const middleElements = [];
   const middleIndex = parseInt(array.length / 2);
@@ -32,7 +12,4 @@ const middle = function(array) {
   return middleElements;
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
