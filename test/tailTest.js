cdbe2c5e75f44a85cdc36593e9aeac94d51d5ee4
goingdust <dustinluck@gmail.com>
2021-11-29 18:23:00 +0000

    Added index.js to store all of our functions in an object so they are easily referenced in other files. Refactored headTest, middleTest, and tailTest so that they now require index and get the key value of its exports object

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,14 @@
 const assert = require('chai').assert;
-const tail = require('../tail');
+const _ = require('../index');
 
 describe ('#tail', () => {
   it ('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    assert.deepEqual(_.tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
   it ('should return [2, 3, 4, 5, 6] for [1, 2, 3, 4, 5, 6]', () => {
-    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
+    assert.deepEqual(_.tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
   });
   it ('should return "ello" for "Hello"', () => {
-    assert.deepEqual(tail('Hello'), 'ello');
+    assert.deepEqual(_.tail('Hello'), 'ello');
   });
 });
